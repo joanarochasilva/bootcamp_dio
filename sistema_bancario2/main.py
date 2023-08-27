7dd39626581469a6a10ecfb4f51968be865ec3cd
Joana Silva <joanarochadasilva96@gmail.com>
2023-08-27 15:57:43 -0300

    Pasta sistema_bancario apagada

---
+++ sistema_bancario2/main.py
@@ -0,0 +1,14 @@
+
+def menu():
+    """"
+    =========================== MENU ===========================
+    1 - Nova Conta"
+    2 - Listar Contas
+    3 - Novo Usuário
+    4 - Depositar
+    5 - Sacar
+    6 - Extrato
+    7 - Sair
+
+    """
+    return input(menu())
