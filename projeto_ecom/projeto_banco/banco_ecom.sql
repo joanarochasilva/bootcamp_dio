b1b00e20fb387149e26cf6f1d932df2776f78665
Joana Silva <joanarochadasilva96@gmail.com>
2023-09-09 12:58:40 -0300

    Refinamento do modelo inicial para e-commerce

--- projeto_ecom/projeto_banco/banco_ecom.sql
+++ projeto_ecom/projeto_banco/banco_ecom.sql
@@ -2,127 +2,180 @@
 CREATE DATABASE ecommerce;
 USE ecommerce;
 
+-- criar tabela pessoa física
+CREATE TABLE naturalPerson
+(
+	idNaturalPerson INT AUTO_INCREMENT PRIMARY KEY,
+    CPF CHAR(11) NOT NULL UNIQUE,
+    fName CHAR(10) NOT NULL,
+    mName CHAR(15),
+    lName CHAR(15) NOT NULL,
+    birthDay DATE NOT NULL
+);
+
+-- criar a tabela pessoa jurídica
+CREATE TABLE legalPerson
+(
+	idLegalPerson INT AUTO_INCREMENT PRIMARY KEY,
+    CNPJ CHAR(15) NOT NULL UNIQUE,
+    corporateName VARCHAR(255)
+);
+
+-- criar tabela usuário
+CREATE TABLE users
+(
+	idUser INT AUTO_INCREMENT PRIMARY KEY,
+    idUserNaturalPerson INT,
+    idUserLegalPerson INT,
+    email VARCHAR(20) NOT NULL UNIQUE,
+    userName VARCHAR(20) NOT NULL UNIQUE,
+    userPassword VARCHAR(10) NOT NULL,
+    registrationDate DATETIME NOT NULL,
+    userType ENUM('Cliente', 'Fornecedor', 'Vendedor') NOT NULL,
+    personType ENUM('Pessoa Juridica', 'Pessoa Física'),
+    CONSTRAINT fk_user_naturalPerson FOREIGN KEY(idUserNaturalPerson) REFERENCES naturalPerson(idNaturalPerson),
+    CONSTRAINT fk_user_legalPerson FOREIGN KEY(idUserLegalPerson) REFERENCES legalPerson(idLegalPerson)
+	ON UPDATE CASCADE
+);
+
+-- criar tabela endereços
+CREATE TABLE adress
+(
+	idAdress INT AUTO_INCREMENT PRIMARY KEY,
+    idAdressUser INT,
+    publicPlace ENUM('Alameda', 'Avenida', 'Condomínio', 'Conjunto', 'Distrito', 'Estrada', 'Ladeira', 'Rua', 'Travessa', 'Vila', 'Outro') NOT NULL,
+    location VARCHAR(30) NOT NULL,
+    numberLocation CHAR(5) DEFAULT 'S/N',
+    adressComplement VARCHAR(20),
+    district VARCHAR(20) NOT NULL,
+    city VARCHAR(15) NOT NULL,
+    CONSTRAINT fk_adress_user FOREIGN KEY(idAdressUser) REFERENCES users(idUser)
+    ON UPDATE CASCADE
+);
+
 -- criar tabela cliente
-CREATE TABLE clients(
-	idClient INT AUTO_INCREMENT PRIMARY KEY,
-    fName VARCHAR(10),
-    mInit CHAR(3),
-    lName VARCHAR(20),
-    CPF CHAR(11) NOT NULL,
-    adress VARCHAR(15),
-    houseNumber INT,
-    district VARCHAR(15),
-    city VARCHAR(10),
-    state VARCHAR(2),
-    CONSTRAINT unique_cpf_client UNIQUE (CPF)
-    );
-
+CREATE TABLE clients
+(
+	idClient INT AUTO_INCREMENT PRIMARY KEY
+);
+-- criar tabela fornecedor
+CREATE TABLE supplier
+(
+	idClient INT AUTO_INCREMENT PRIMARY KEY
+);
+-- criar tabela vendedor
+CREATE TABLE seller
+(
+	idSeller INT AUTO_INCREMENT PRIMARY KEY
+);
 -- criar tabela produto
--- size = dimensão do produto
-CREATE TABLE product(
+CREATE TABLE product 
+(
 	idProduct INT AUTO_INCREMENT PRIMARY KEY,
-    pName VARCHAR(10),
-    classification_kids BOOL DEFAULT FALSE,
-    category ENUM('Eletrônico', 'Vestimenta', 'Brinquedos,', 'Alimentos', 'Móveis') NOT NULL,
-    avaliação FLOAT DEFAULT 0,
+    productName VARCHAR(45) NOT NULL,
+    kidsProduct BOOL DEFAULT FALSE,
+    category ENUM('Móveis', 'Eletrodomesticos', 'Eletrônico', 'Roupas', 'Alimentos') NOT NULL,
+    rating FLOAT(5) DEFAULT 0,
     size VARCHAR(10)
-    );
-
+);
+-- criar tabela tipo de pagamento
+CREATE TABLE payment 
+(
+	idPayment INT AUTO_INCREMENT PRIMARY KEY,
+    idPixPayment INT,
+    idCardPayment INT,
+    idSlipPayment INT,
+    paymentData DATETIME NOT NULL,
+    paymentType ENUM('PIX', 'Cartão', 'Boleto'),
+    CONSTRAINT fk_pix_payment FOREIGN KEY(idPixPayment) REFERENCES pixPayment(idPix),
+    CONSTRAINT fk_card_payment FOREIGN KEY(idCardPayment) REFERENCES cardPayment(idCard),
+    CONSTRAINT fk_slip_payment FOREIGN KEY(idSlipPayment) REFERENCES slipPayment(idSlip)
+    ON UPDATE CASCADE
+);
+-- criar tabela PIX
+CREATE TABLE pixPayment 
+(
+	idPix INT AUTO_INCREMENT PRIMARY KEY,
+    pixCod VARCHAR(32) NOT NULL UNIQUE,
+    validity DATE NOT NULL
+);
+-- criar tabela cartão
+CREATE TABLE cardPayment 
+(
+	idCard INT AUTO_INCREMENT PRIMARY KEY,
+    paymentNumber INT(16) NOT NULL,
+    nameCard CHAR(15) NOT NULL,
+    expirationDateCard DATE NOT NULL,
+    cvv INT(3) NOT NULL
+);
+-- criar tabela boleto
+CREATE TABLE slipPayment 
+(
+	idSlip INT AUTO_INCREMENT PRIMARY KEY,
+    slipNumber INT(48) NOT NULL,
+    expirationDateSlip DATE NOT NULL
+);
 -- criar tabela pedido
-CREATE TABLE request(
+CREATE TABLE request 
+(
 	idRequest INT AUTO_INCREMENT PRIMARY KEY,
     idRequestClient INT,
-    requestStatus ENUM('Cancelado', 'Confirmado', 'Em processamento') DEFAULT 'Em processamento',
-	requestDescription VARCHAR(255),
-    sendValue FLOAT DEFAULT 10,
-    paymentCash BOOL DEFAULT FALSE,
-    CONSTRAINT fk_request_client FOREIGN KEY(idRequestClient) REFERENCES clients(idClient)
-				ON UPDATE CASCADE
-    );
-    
--- criar tabela pagamentos
--- para ser continuado no desafio: termine de implementar a tablea e crie a conhexão com as tabelas necessárias
--- além disso, reflita essa modificação no esquema relacional
--- criar constraints relacionadas ao pagamento
-CREATE TABLE payments(
-	idPClient INT,
-    idPayment INT,
-    typePayment ENUM('Boleto', 'Cartão', 'Pix'),
-    limitAvaible FLOAT,
-    PRIMARY KEY(idPClient, idPayment),
-    CONSTRAINT fk_payment_client FOREIGN KEY(idPClient) REFERENCES clients(idClient),
-    CONSTRAINT fk_payment_resquest FOREIGN KEY(idPayment) REFERENCES request(idRequest)
+    idRequestPayment INT,
+    requestStatus ENUM('Cancelado', 'Confirmado', 'Em processamento') NOT NULL,
+    requestDescription VARCHAR(255),
+    orderShipping FLOAT DEFAULT 10,
+    requestPayment BOOL DEFAULT FALSE,
+    CONSTRAINT fk_request_client FOREIGN KEY(idRequestClient) REFERENCES clients(idClient),
+    CONSTRAINT fk_request_payment FOREIGN KEY(idRequestPayment) REFERENCES payment(idPayment)
+    ON UPDATE CASCADE
 );
-    
--- criar tabela estoque
-CREATE TABLE productsStorage(
-	idProdStorage INT AUTO_INCREMENT PRIMARY KEY,
-	storageLocation VARCHAR(255),
-    Quantity INT DEFAULT 0
+-- criar table estoque
+CREATE TABLE storages 
+(
+	idStorage INT AUTO_INCREMENT PRIMARY KEY,
+    idStorageAdress INT,
+    quantity INT NOT NULL,
+	CONSTRAINT fk_storage_adress FOREIGN KEY(idStorageAdress) REFERENCES adress(idAdress)
 );
-
--- cria tabela fornecedor 
-CREATE TABLE supplier(
-	idSupplier INT AUTO_INCREMENT PRIMARY KEY,
-    socialName VARCHAR(255) NOT NULL,
-    CNPJ CHAR(15) NOT NULL,
-    contact VARCHAR(11) DEFAULT 0,
-    CONSTRAINT unique_suplier UNIQUE(CNPJ)
-);
-
-
--- criar tabela vendedor
-CREATE TABLE seller(
-	idSeller INT AUTO_INCREMENT PRIMARY KEY,
-    socialName VARCHAR(255) NOT NULL,
-    AbstName VARCHAR(255),
-    CNPJ CHAR(15) NOT NULL,
-    CPF CHAR(9),
-    sellerLocation VARCHAR(255),
-    contact VARCHAR(11) DEFAULT 0,
-    CONSTRAINT unique_scnpj_seller UNIQUE(CNPJ),
-    CONSTRAINT unique_scpf_seller UNIQUE(CPF)
-);
-
-
-CREATE TABLE productSeller (
-	idPSeller INT,
-    idProduct INT,
+-- criar tabela vendedor/produto
+CREATE TABLE sellerProduct 
+(
+	idSellerProduct INT,
+    idProductSeller INT,
     prodQuantity INT DEFAULT 1,
-    PRIMARY KEY (idPseller, idProduct),
-    CONSTRAINT fk_product_seller FOREIGN KEY(idPSeller) REFERENCES seller(idSeller),
-    CONSTRAINT fk_product_seller_product FOREIGN KEY (idProduct) REFERENCES product(idProduct)
+    PRIMARY KEY (idSellerProduct, idProductSeller),
+    CONSTRAINT fk_product_seller FOREIGN KEY(idSellerProduct) REFERENCES seller(idSeller),
+    CONSTRAINT fk_product_seller_product FOREIGN KEY (idProductSeller) REFERENCES product(idProduct)
 );
-
-CREATE TABLE productSupplier (
-	idPSupplier INT,
-    idProduct INT,
+-- criar tabela fornecedor/produto
+CREATE TABLE supplierProduct 
+(
+	idProductSupplier INT,
+    idSuplierProduct INT,
     quantity INT,
-    PRIMARY KEY (idPSupplier, idProduct),
-    CONSTRAINT fk_product_supplier FOREIGN KEY(idPSupplier) REFERENCES supplier(idSupplier),
-    CONSTRAINT fk_product_supplier_product FOREIGN KEY (idProduct) REFERENCES product(idProduct)
+    PRIMARY KEY (idProductSupplier, idSupllierProduct),
+    CONSTRAINT fk_product_supplier FOREIGN KEY(idProductSupplier) REFERENCES supplier(idSupplier),
+    CONSTRAINT fk_product_supplier_product FOREIGN KEY (idSupplierProduct) REFERENCES product(idProduct)
 );
-
-CREATE TABLE productRequest (
-	idPRproduct INT,
-    idPResquest INT,
+-- criar tabela pedido/produto
+CREATE TABLE requestProduct 
+(
+	idRequestProduct INT,
+    idProductRequest INT,
     prQuantity INT DEFAULT 0,
     poStatus ENUM('Disponível', 'Sem estoque') DEFAULT 'Disponível',
-    PRIMARY KEY (idPRproduct, idPResquest),
-    CONSTRAINT fk_product_request FOREIGN KEY (idPRproduct) REFERENCES product(idProduct),
-    CONSTRAINT fk_product_request_product FOREIGN KEY (idPResquest) REFERENCES request(idRequest)
+    PRIMARY KEY (idRequestProduct, idProductRequest),
+    CONSTRAINT fk_product_request FOREIGN KEY (idRequestProduct) REFERENCES product(idProduct),
+    CONSTRAINT fk_product_request_product FOREIGN KEY (idProductRequest) REFERENCES request(idRequest)
 );
-
-
-CREATE TABLE locationStorage (
-	idLproduct INT,
-    idLstorage INT,
+-- criar tabela estoque/produto
+CREATE TABLE storageProduct 
+(
+	idStorageProduct INT,
+    idProductStorage INT,
     location VARCHAR(255) NOT NULL,
-    PRIMARY KEY (idLproduct, idLstorage),
-    CONSTRAINT fk_product_storage FOREIGN KEY (idLproduct) REFERENCES product(idProduct),
-    CONSTRAINT fk_product_locationStorage_product FOREIGN KEY (idLstorage) REFERENCES productsStorage(idProdStorage)
+    PRIMARY KEY (idStorageProduct, idProductStorage),
+    CONSTRAINT fk_product_storage FOREIGN KEY (idStorageProduct) REFERENCES product(idProduct),
+    CONSTRAINT fk_product_locationStorage_product FOREIGN KEY (idProductStorage) REFERENCES storages(idStorage)
 );
-
-
-
 
